d63da38bd5445e9e3288332bf75ede9fe494ee97
Logan Emerson <logemerson@gmail.com>
2019-06-26 15:08:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  const newDriver = Object.assing(driver, [key]: value)
+  const newDriver = Object.assing(driver, { [key]: value)});
   
   return newDriver;
 }
