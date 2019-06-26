f4df293840f0ddab3f8fa688219194acc6e9b824
Logan Emerson <logemerson@gmail.com>
2019-06-26 15:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   const newDriver = { ...driver };
   
-  newDriver.key = value;
+  newDriver[key] = value;
   return newDriver;
 }
